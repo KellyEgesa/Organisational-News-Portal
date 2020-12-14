40dd448b308b672a05c8d1e3aa7c5b323b88c595
KellyEgesa <kelly.egesa@gmail.com>
2020-12-14 16:08:09 +0300

    added the drop.sql file

--- create.sql
+++ create.sql
@@ -3,4 +3,4 @@ CREATE DATABASE organisation_news;
 CREATE TABLE users (id SERIAL PRIMARY KEY, userName VARCHAR,positionInCompany VARCHAR, userRole VARCHAR,departmentId INTEGER);
 CREATE TABLE news (id SERIAL PRIMARY KEY, newsInfo VARCHAR, type VARCHAR,departmentId INTEGER);
 CREATE TABLE department (id SERIAL PRIMARY KEY, departmentName VARCHAR, departmentDescription VARCHAR,departmentEmployees INTEGER);
-CREATE DATABASE organisation_news_tests WITH TEMPLATE organisation_news;
+CREATE DATABASE organisation_news_test WITH TEMPLATE organisation_news;
