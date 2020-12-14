e8e767071b881faba8c4e64a2b74628ce008ec47
KellyEgesa <kelly.egesa@gmail.com>
2020-12-14 21:42:54 +0300

    added all tests for sql20Users and passed them

---
+++ src/main/java/Dao/Sql2oUser.java
@@ -0,0 +1,63 @@
+package Dao;
+
+import model.User;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class Sql2oUser implements UserDao {
+    @Override
+    public void saveUser(User user) {
+        String sql = "INSERT INTO users (userName, positionInCompany, userRole, departmentId) VALUES (:userName, :positionInCompany,:userRole ,:departmentId)";
+        try (Connection con = DB.sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .addParameter("userName", user.getUserName())
+                    .addParameter("positionInCompany", user.getPositionInCompany())
+                    .addParameter("userRole", user.getUserRole())
+                    .addParameter("departmentId", user.getDepartmentId())
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        String sql = "SELECT * FROM users";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .executeAndFetch(User.class);
+        }
+    }
+
+    @Override
+    public User findUserById(int id) {
+        String sql = "SELECT * FROM users WHERE id = :id";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(User.class);
+        }
+    }
+
+    @Override
+    public void deleteUserById(int id) {
+        String sql = "DELETE FROM users WHERE id =:id";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+
+    }
+
+    @Override
+    public void clearUsers() {
+        String sql = "DELETE FROM users";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .executeUpdate();
+        }
+
+    }
+}
--- src/main/java/Dao/UserDao.java
+++ src/main/java/Dao/UserDao.java
@@ -1,4 +1,13 @@
 package Dao;
 
+import model.User;
+
+import java.util.List;
+
 public interface UserDao {
+    void saveUser(User user);
+    List<User> getAllUsers();
+    User findUserById(int id);
+    void deleteUserById(int id);
+    void clearUsers();
 }
